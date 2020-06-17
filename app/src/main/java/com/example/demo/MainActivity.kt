bbc8f84d14cc3647060d0de86e4136f0b6569dbc
Mcbraingh <benjamintackie@ymail.com>
2020-06-17 03:11:47 -0700

    still comments

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    // main class
+    // starting point
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
